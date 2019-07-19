059e9587d7433d82888d59441a2d393e4082353a
kevinhuang78 <kevin_huang_iim@yahoo.com>
2019-07-19 15:59:53 +0200

    feat: allow redux logger on dev only

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import logger from 'redux-logger';
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(logger, thunk)
+    process.env.NODE_ENV === "production" ? applyMiddleware(thunk) : applyMiddleware(logger, thunk)
 );
 
 ReactDOM.render(
